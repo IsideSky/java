990bbf5c0fa1f5fbcd9709fcb281e1d6776774bf
Sunshine <Sunshine@DESKTOP-UBTQAOS.lan>
2024-09-27 15:52:04 +0200

    add ciao mondo

--- src/main/java/it/generationitaly/app/App.java
+++ src/main/java/it/generationitaly/app/App.java
@@ -5,10 +5,14 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		metodo1();
+		metodo2();
 	}
 	
 	private static void metodo1(){
 		System.out.println("Ciao");
 	}
-
+	
+	private static void metodo2(){
+		System.out.println("Ciao mondo");
+	}
 }
